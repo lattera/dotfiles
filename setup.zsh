2b54b1b96b455632ab17cede8499ebed7d7ca85a
Shawn Webb <lattera@gmail.com>
2013-08-26 15:05:27 -0400

    Add i3status config

--- setup.zsh
+++ setup.zsh
@@ -7,6 +7,7 @@ cd ~
 rm -f .profile
 rm -f .tmux.conf
 rm -f .zshrc
+rm -f .i3/config
 
 if [ ! -d .vim ] && [ ! ${#GIT} -eq 0 ] || [ ! -f .vimrc ] ; then
     rm -f .vimrc
@@ -26,6 +27,12 @@ fi
 
 sed "s,__PLUGINS__,${1}," ~/dotfiles/zshrc.templ > ~/dotfiles/zshrc
 
+if [ ! -d .i3 ]; then
+    mkdir -p .i3
+fi
+
+ln -s ~/dotfiles/i3status.conf .i3status.conf
+ln -s ~/dotfiles/i3config .i3/config
 ln -s ~/dotfiles/.freebsd_profile .profile
 ln -s ~/dotfiles/.tmux.conf .tmux.conf
 ln -s ~/dotfiles/zshrc .zshrc
