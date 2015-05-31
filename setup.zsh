f6f92c142fcbd7a3562971efac3aadfbbea62b14
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-05-31 15:38:58 -0400

    Give some love to my dotfiles

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 GIT=$(which git)
+os=$(uname)
+os=${os:l}
 
 cd ~
 
@@ -8,6 +10,7 @@ rm -f .profile
 rm -f .tmux.conf
 rm -f .zshrc
 rm -f .i3/config
+rm -f .i3status.conf
 rm -f .muttrc
 
 if [ ! -d .vim ] && [ ! ${#GIT} -eq 0 ] || [ ! -f .vimrc ] ; then
@@ -34,14 +37,32 @@ if [ ! -d .oh-my-zsh ]; then
     git clone git://github.com/lattera/oh-my-zsh.git .oh-my-zsh
 fi
 
-sed "s,__PLUGINS__,${1}," ~/dotfiles/zshrc.templ > ~/dotfiles/zshrc
+plugins=""
+while getopts 'p:' o; do
+	case "${o}" in
+	p)
+		if [ ! -d .oh-my-zsh/plugins/${OPTARG} ]; then
+			echo "Warning: oh-my-zsh plugin ${OPTARG} not found"
+		fi
+
+		plugins="${plugins} ${OPTARG}"
+		;;
+	*)
+		echo "USAGE: ${0} [-p plugin]"
+		echo "    -p plugin        Add zsh plugin. May be specified multiple times"
+		exit 1
+		;;
+	esac
+done
+
+sed "s,__PLUGINS__,${plugins}," ~/dotfiles/zshrc.templ > ~/dotfiles/zshrc
 
 if [ ! -d .i3 ]; then
     mkdir -p .i3
 fi
 
-ln -s ~/dotfiles/i3status.conf .i3status.conf
-ln -s ~/dotfiles/i3config .i3/config
+ln -s ~/dotfiles/i3status.${os}.conf .i3status.conf
+ln -s ~/dotfiles/i3config.${os} .i3/config
 ln -s ~/dotfiles/.freebsd_profile .profile
 ln -s ~/dotfiles/.tmux.conf .tmux.conf
 ln -s ~/dotfiles/zshrc .zshrc
